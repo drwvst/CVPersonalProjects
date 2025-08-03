03539cfe3307a66fa827ac2a293704b58f94d28f
drwvst <drewjvest@gmail.com>
2025-08-02 19:55:08 -0500

    Set up and learned about different libraries I can use here. I'm going to use OpenCV for image processing and manipulation and I'm going to use tkinter for a simple GUI

---
+++ pencil_sketch_converter.py
@@ -0,0 +1,9 @@
+import cv2 # OpenCV library
+import numpy as np # Scientific computing
+import tkinter as tk # Standard GUI toolKit
+from tkinter import filedialog #provides dialogs for opening or saving files
+from tkinter import messagebox # simple pop-up message windows for info, warnings, and errors
+from PIL import Image,ImageTk # Image is uesd to open, manipulate, and save images
+# ImageTk is a module to convert Pillow images into a format for the GUI to understand
+
+
